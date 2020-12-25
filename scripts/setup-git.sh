f2741128fb739fe2b4d210e18ee2df16871f1d40
micaelviana <44692237+micaelviana@users.noreply.github.com>
2020-12-25 10:33:27 -0400

    Update setup-git.sh

--- scripts/setup-git.sh
+++ scripts/setup-git.sh
@@ -25,37 +25,6 @@
 # git reset --hard orgin/master
 
 
-# installing git if not installed for specific distro's
-
-if ! location="$(type -p "git")" || [ -z "git" ]; then
-
-	echo "#################################################"
-	echo "installing git for this script to work"
-	echo "#################################################"
-
-  	sudo apt install git -y
-	# check if apt-git is installed
-	if which apt-get > /dev/null; then
-
-		sudo apt-get install -y git
-
-	fi
-
-	# check if pacman is installed
-	if which pacman > /dev/null; then
-
-		sudo pacman -S --noconfirm git
-
-	fi
-
-	# check if eopkg is installed
-	if which eopkg > /dev/null; then
-
-		sudo eopkg -y install git
-
-	fi
-
-
 #setting up git
 #https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
 
