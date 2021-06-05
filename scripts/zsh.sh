1f5500e55417ef64970843754a21b7e5c7b7f19f
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-06-05 12:05:50 -0400

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,9 +2,3 @@
 
 # Plugin Manager for ZSH
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
-
-#Repo from Spacehimp prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-
-#Symlink spaceship.zsh-theme to your oh-my-zsh custom themes directory:
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" 
