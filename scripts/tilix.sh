3ec123bae9236dcf7b33db2b3df4c2098fc0fd77
micaelviana <micael.vianasilva@gmail.com>
2021-06-23 18:28:03 -0400

    change tiix theme

--- scripts/tilix.sh
+++ scripts/tilix.sh
@@ -4,4 +4,4 @@
 # "Downloading Argonaut theme
 mkdir $HOME/.config/tilix
 mkdir $HOME/.config/tilix/schemes
-wget -qO $HOME"/.config/tilix/schemes/argonaut.json" https://git.io/v7QV5
+wget -qO $HOME"/.config/tilix/schemes/one-dark.json" https://git.io/v7Qaw
