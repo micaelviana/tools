a5f5ccf7c65235fcea0d6042f1bdcf5953addc78
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-09-11 10:53:33 -0400

    add pack: caffeine-ng

--- packages/aur.py
+++ packages/aur.py
@@ -1,6 +1,6 @@
 import os
 
-packages = ['asdf-vm','brave-bin','google-chrome','visual-studio-code-bin']
+packages = ['asdf-vm','brave-bin','caffeine-ng','google-chrome','visual-studio-code-bin']
 
 for item in packages:
     command = "yay -S "+item+" --needed"
