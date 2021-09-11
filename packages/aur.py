a5452a5fcc5ae147c21010d2cc7e3e9bacadcff8
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-09-11 10:39:48 -0400

    add pack: asdf

--- packages/aur.py
+++ packages/aur.py
@@ -1,6 +1,6 @@
 import os
 
-packages = ['brave-bin','google-chrome','visual-studio-code-bin']
+packages = ['asdf-vm','brave-bin','google-chrome','visual-studio-code-bin']
 
 for item in packages:
     command = "yay -S "+item+" --needed"
