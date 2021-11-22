a4c3e983b855b5685dbfa16ae13b626a77c57902
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-11-22 11:21:41 -0400

    remove package:asdf

--- packages/aur.py
+++ packages/aur.py
@@ -1,6 +1,6 @@
 import os
 
-packages = ['asdf-vm','brave-bin','caffeine-ng','google-chrome','visual-studio-code-bin']
+packages = ['brave-bin','caffeine-ng','google-chrome','visual-studio-code-bin']
 
 for item in packages:
     command = "yay -S "+item+" --needed"
