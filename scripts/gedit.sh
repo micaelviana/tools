d537cf9812591012ea486202291bfbdac87c2429
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-12-23 11:11:22 -0400

    Delete gedit.sh

--- scripts/gedit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://raw.githubusercontent.com/dracula/gedit/master/dracula.xml -P /tmp
-mkdir $HOME/.local/share/gedit/      
-mkdir $HOME/.local/share/gedit/styles
-mv /tmp/dracula.xml $HOME/.local/share/gedit/styles/
