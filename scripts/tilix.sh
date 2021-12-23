1ccbdcd1ef9b4e269ab71fc1bb3f40667cf70645
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-12-23 11:19:45 -0400

    Delete tilix.sh

--- scripts/tilix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#TILIX
-
-# -----TILIX
-# "Downloading Argonaut theme
-mkdir $HOME/.config/tilix
-mkdir $HOME/.config/tilix/schemes
-wget -qO $HOME"/.config/tilix/schemes/one-dark.json" https://git.io/v7Qaw
