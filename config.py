41b635421ba379bcb37dfcd63e6e97ccc500dcb4
micaelviana <44692237+micaelviana@users.noreply.github.com>
2022-02-28 18:14:47 -0400

    zsh - finished config.py - added some help info

--- config.py
+++ config.py
@@ -11,10 +11,17 @@ flags = {
 
 if __name__ == "__main__":
     #the program assumes that the python filename is the first argument, so I cant start at the beggining
-    for i in range(1,len(sys.argv)):
-        flag=sys.argv[i]
-        if flag in flags:
-            argument=flags[flag]
-            command="bash scripts/"+argument
-            os.system(command)
+    if(len(sys.argv)>1):
+        for i in range(1,len(sys.argv)):
+            flag=sys.argv[i]
+            if flag in flags:
+                argument=flags[flag]
+                command="bash scripts/"+argument
+                os.system(command)
+    else:
+        print("Available options:")
+        print("python config.py -g will run git.sh script")
+        print("python config.py -d will run directories.sh script")
+        print("More than one can be used:")
+        print("python config.py -d -g")
 
