af46903ce365b6830d8fe99a81abcb81657656a6
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-03-06 09:30:44 -0400

    Update directories.sh

--- scripts/directories.sh
+++ scripts/directories.sh
@@ -6,5 +6,8 @@
 #create folder to download binaries
 [ ! -d $HOME/.local/bin ] && mkdir -p $HOME/.local/bin
 
+#create config folder
+[ ! -d $HOME/.config ] && mkdir -p $HOME/.config
+
 echo "programming folder is ready"
 echo "local/bin folder is ready"
