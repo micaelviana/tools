543ce548754a3aca5bbc681a323201c14bfce0f0
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-03-06 09:25:20 -0400

    Update arch.py

--- packages/arch.py
+++ packages/arch.py
@@ -25,5 +25,5 @@ packages = ['base-devel',
 ]
 
 for item in packages:
-    command = 'sudo pacman -S '+item+' --needed --noconfirm'
+    command = 'sudo pacman -S '+item+' --needed'
     os.system(command)
