ef15903e572f99bc0e1563d466638907a8e3aab2
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-01-02 14:48:53 -0400

    Update aur.py

--- packages/aur.py
+++ packages/aur.py
@@ -1,7 +1,8 @@
 import os
 
 packages = ['brave-bin','caffeine-ng','google-chrome','mintstick']
+temp=['onedrive-abraunegg','fnm-bin']
 
-for item in packages:
+for item in temp:
     command = "yay -S "+item+" --needed"
     os.system(command)
