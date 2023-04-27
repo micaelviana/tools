9d2ca49ef3cf835e6ab8fd5f68928cb702d6929a
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-04-27 14:20:12 -0400

    Create zsh.sh

---
+++ scripts/zsh.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zap-zsh/completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/completions
+
+git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autopair
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
