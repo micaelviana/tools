bb3c09aa26200ca13681b28f500be46b645efc4d
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-04-29 14:04:39 -0400

    remove zsh-autopair

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -4,6 +4,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 git clone https://github.com/zap-zsh/completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/completions
 
-git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autopair
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
