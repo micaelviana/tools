40b77b677f9dfd2ce5319889f7d2e7a63e2ac0da
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-04-29 17:28:26 -0400

    Update directories.sh

--- scripts/directories.sh
+++ scripts/directories.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 #create programming folder
-[ ! -d $HOME/Developer ] && mkdir -p $HOME/Developer/{poc,projects,repos} && echo "developer folder is ready"
+[ ! -d $HOME/Developer ] && mkdir -p $HOME/Developer/{poc,projects,repos} 
+echo "developer folder is ready"
 
 #create folder to download binaries
-[ ! -d $HOME/.local/bin ] && mkdir -p $HOME/.local/bin && echo "local/bin folder is ready"
+[ ! -d $HOME/.local/bin ] && mkdir -p $HOME/.local/bin && 
+echo "local/bin folder is ready"
 
 #create config folder
-[ ! -d $HOME/.config ] && mkdir -p $HOME/.config && echo "config folder is ready" 
+[ ! -d $HOME/.config ] && mkdir -p $HOME/.config && 
+echo "config folder is ready" 
