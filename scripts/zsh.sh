277f6d7aaa93fc6dfcd986abca127bd266d646e2
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-06-25 10:36:03 -0400

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,6 +2,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/zap-zsh/completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/completions
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
