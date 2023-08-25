6ba1aafc20f9971dd62ecfbad6223232db9f04c5
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-08-24 22:13:15 -0400

    remove aur packages

--- packages/aur.py
+++
@@ -1,8 +0,0 @@
-import os
-
-packages = ['brave-bin','caffeine-ng','google-chrome','mintstick']
-temp = ['fnm-bin']
-
-for item in temp:
-    command = "yay -S "+item+" --needed"
-    os.system(command)
